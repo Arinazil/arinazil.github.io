a3e3d4948a97ebea8900a61e0cab4f2b408468ec
Vaeril <marleysudbury@gmail.com>
2016-04-15 15:33:06 +0100

    fixed menu

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 document.getElementById("nav01").innerHTML =
 "<nav>"+
-"<a href=''>Home</a> | "+
-  "<a href='news'>News</a> | "+
-  "<a href='contact'>Contact</a> | "+
-  "<a href='about'>About</a> | "+
+"<a href='http://www.arinazil.co.uk'>Home</a> | "+
+  "<a href='http://www.arinazil.co.uk/news'>News</a> | "+
+  "<a href='http://www.arinazil.co.uk/contact'>Contact</a> | "+
+  "<a href='http://www.arinazil.co.uk/about'>About</a> | "+
   "<div class='dropdown'>"+
-  "<a href='games'>Games</a>"+
+  "<a href='http://www.arinazil.co.uk/games'>Games</a>"+
   "<div class='dropdown-content'>"+
-    "<p><a href='games/banished'>Banished</a></p>"+
+    "<p><a href='http://www.arinazil.co.uk/games/banished'>Banished</a></p>"+
   "</div>"+
   "</div> | "+
   "<div class='dropdown'>"+
-  "<a href='gallery'>Gallery</a>"+
+  "<a href='http://www.arinazil.co.uk/gallery'>Gallery</a>"+
   "<div class='dropdown-content'>"+
-    "<p><a href='gallery/banished1'>Banished 1</a></p>"+
+    "<p><a href='http://www.arinazil.co.uk/gallery/banished1'>Banished 1</a></p>"+
   "</div>"+
   "</div>"+
 "</nav>";
