a973c3a0b6b5f7bec30a1904bca1b16f20c56f44
Vaeril <marleysudbury@gmail.com>
2016-04-15 20:55:02 +0100

    Menu JS
    and dropdowns finally working in javascript

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
+function setActive() {
+  aObj = document.getElementById('nav').getElementsByTagName('a');
+  for(i=0;i<aObj.length;i++) { 
+    if(document.location.href.indexOf(aObj[i].href)>=0) {
+      aObj[i].className='active';
+    }
+  }
+}
+window.onload = setActive;
+
 document.getElementById("nav01").innerHTML =
 "<nav>"+
-"<a href='http://www.arinazil.co.uk'>Home</a> | "+
-  "<a href='http://www.arinazil.co.uk/news'>News</a> | "+
-  "<a href='http://www.arinazil.co.uk/contact'>Contact</a> | "+
-  "<a href='http://www.arinazil.co.uk/about'>About</a> | "+
-  "<div class='dropdown'>"+
-  "<a href='http://www.arinazil.co.uk/games'>Games</a>"+
-  "<div class='dropdown-content'>"+
-    "<p><a href='http://www.arinazil.co.uk/games/banished'>Banished</a></p>"+
-  "</div>"+
-  "</div> | "+
-  "<div class='dropdown'>"+
-  "<a href='http://www.arinazil.co.uk/gallery'>Gallery</a>"+
-  "<div class='dropdown-content'>"+
-    "<p><a href='http://www.arinazil.co.uk/gallery/banished1'>Banished 1</a></p>"+
-  "</div>"+
-  "</div>"+
+  "<li><a href='http://www.arinazil.co.uk'>Home</a></li>"+
+  "<li><a href='http://www.arinazil.co.uk/news'>News</a></li>"+
+  "<li><a href='http://www.arinazil.co.uk/contact'>Contact</a></li>"+
+  "<li><a href='http://www.arinazil.co.uk/about'>About</a></li>"+
+  "<li class='dropdown'>"+
+    "<a href='http://www.arinazil.co.uk/games' class='dropbtn'>Games</a>"+
+    "<div class='dropdown-content'>"+
+      "<a href='http://www.arinazil.co.uk/games/banished'>Banished</a>"+
+    "</div>"+
+  "</li>"+
+  "<li class='dropdown'>"+
+    "<a href='http://www.arinazil.co.uk/gallery' class='dropbtn'>Galleries</a>"+
+    "<div class='dropdown-content'>"+
+      "<a href='http://www.arinazil.co.uk/gallery/banished1'>Banished 1</a>"+
+    "</div>"+
+  "</li>"+
+  "<li><a href='http://www.arinazil.co.uk/testing'>Testing</a></li>";
 "</nav>";
