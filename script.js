269267f56958e75061aa1e118c930773499a4d50
Vaeril <marleysudbury@gmail.com>
2016-04-15 21:02:08 +0100

    nav active

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function setActive() {
-  aObj = document.getElementById('nav').getElementsByTagName('a');
+  aObj = document.getElementById('nav01').getElementsByTagName('a');
   for(i=0;i<aObj.length;i++) { 
     if(document.location.href.indexOf(aObj[i].href)>=0) {
       aObj[i].className='active';
