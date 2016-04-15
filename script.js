4bca08b6aefaca6b110d588dab5c8ebef22402b4
Vaeril <marleysudbury@gmail.com>
2016-04-15 15:30:28 +0100

    Javascript menu

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById("nav01").innerHTML =
+"<nav>"+
+"<a href=''>Home</a> | "+
+  "<a href='news'>News</a> | "+
+  "<a href='contact'>Contact</a> | "+
+  "<a href='about'>About</a> | "+
+  "<div class='dropdown'>"+
+  "<a href='games'>Games</a>"+
+  "<div class='dropdown-content'>"+
+    "<p><a href='games/banished'>Banished</a></p>"+
+  "</div>"+
+  "</div> | "+
+  "<div class='dropdown'>"+
+  "<a href='gallery'>Gallery</a>"+
+  "<div class='dropdown-content'>"+
+    "<p><a href='gallery/banished1'>Banished 1</a></p>"+
+  "</div>"+
+  "</div>"+
+"</nav>";
