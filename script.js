ecfbdef9556bbea2a21a99aabdae8a359ead801f
Marley Sudbury <marleysudbury@gmail.com>
2017-01-16 23:03:31 +0000

    Changed nav links to GitHub.io
    Cause they didn’t work

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 document.getElementById("nav01").innerHTML =
 "<nav>"+
-  "<li><a href='http://www.arinazil.co.uk'>Home</a></li>"+
-  "<li><a href='http://www.arinazil.co.uk/news'>News</a></li>"+
-  "<li><a href='http://www.arinazil.co.uk/contact'>Contact</a></li>"+
-  "<li><a href='http://www.arinazil.co.uk/about'>About</a></li>"+
+  "<li><a href='https://arinazil.github.io'>Home</a></li>"+
+  "<li><a href='https://arinazil.github.io/news'>News</a></li>"+
+  "<li><a href='https://arinazil.github.io/contact'>Contact</a></li>"+
+  "<li><a href='https://arinazil.github.io/about'>About</a></li>"+
   "<li class='dropdown'>"+
-    "<a href='http://www.arinazil.co.uk/games' class='dropbtn'>Games</a>"+
+    "<a href='https://arinazil.github.io/games' class='dropbtn'>Games</a>"+
     "<div class='dropdown-content'>"+
-      "<a href='http://www.arinazil.co.uk/games/banished'>Banished</a>"+
+      "<a href='https://arinazil.github.io/games/banished'>Banished</a>"+
     "</div>"+
   "</li>"+
   "<li class='dropdown'>"+
-    "<a href='http://www.arinazil.co.uk/gallery' class='dropbtn'>Galleries</a>"+
+    "<a href='https://arinazil.github.io/gallery' class='dropbtn'>Galleries</a>"+
     "<div class='dropdown-content'>"+
-      "<a href='http://www.arinazil.co.uk/gallery/banished1'>Banished 1</a>"+
+      "<a href='https://arinazil.github.io/gallery/banished1'>Banished 1</a>"+
     "</div>"+
   "</li>"+
-  "<li><a href='http://www.arinazil.co.uk/testing'>Testing</a></li>";
+  "<li><a href='https://arinazil.github.io/testing'>Testing</a></li>";
 "</nav>";
 
 $(function() {
